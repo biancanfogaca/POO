778a62bb0dde4bb1f5bbdd2b9bff714eaad5fda3
Bianca Fogaça <biancanfogaca@outlook.com>
2023-09-03 22:06:15 -0400

    ProjetoEstagiario

---
+++ projetoestagiario/app.java
@@ -0,0 +1,13 @@
+package poo.projetoestagiario;
+
+import java.time.LocalDate;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+    
+        Funcionario f1 = new Funcionario();
+
+        Passageiro p1 = new Passageiro();
+
+    }
+}
